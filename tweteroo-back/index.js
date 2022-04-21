0cb6f138a0d43179864bcaefc1ec18fe92dc93da
heusyfelype <felype.heusy@gmail.com>
2022-04-21 17:55:54 -0300

    Adicionada funcionalidade: Post -- sign-up

--- tweteroo-back/index.js
+++ tweteroo-back/index.js
@@ -3,3 +3,14 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+
+let user = [];
+
+app.post("/sign-up", (req, res) =>{
+    user.push(req.query);
+    console.log(user)
+    res.send("Deu tudo certo")
+});
+
+
+app.listen(5000);
