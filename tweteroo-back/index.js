928989eeb08ad8618862f4fe93831a4b629b5130
heusyfelype <felype.heusy@gmail.com>
2022-04-21 19:31:13 -0300

    Adicionada funcionalidade: get -- imprimir twits do servidor

--- tweteroo-back/index.js
+++ tweteroo-back/index.js
@@ -5,12 +5,34 @@ const app = express();
 app.use(cors());
 
 let user = [];
+let tweet = [];
+let tweets = [{
+    username: "bobesponja",
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "eu amo o hub"
+}];
 
-app.post("/sign-up", (req, res) =>{
+app.post("/sign-up", (req, res) => {
     user.push(req.query);
-    console.log(user)
-    res.send("Deu tudo certo")
+    console.log("Sign-up funcionando");
+    res.send("Sign-up funcionando");
 });
+
+app.get("/tweets", (req, res) => {
+    let tweetsToReturn = [];
+    if(tweets.length <= 10){
+        for(let i = tweets.length; i>0; i--){
+            tweetsToReturn.push(tweets[i-1])
+        }
+    } else{
+        for(let i = tweets.length; i > tweets.length - 10; i--){
+            tweetsToReturn.push(tweets[i-1])
+        }
+    }
+    
+    console.log(tweetsToReturn, tweets.length)
+    res.send(tweetsToReturn)
+})
 
 
 app.listen(5000);
