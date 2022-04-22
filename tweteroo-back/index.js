698b8553b1a7634981c3da7ff3315798f8c21d73
heusyfelype <felype.heusy@gmail.com>
2022-04-22 16:34:18 -0300

    Adicionada funcionalidade: post -- usuario pode postar o tweet agora

--- tweteroo-back/index.js
+++ tweteroo-back/index.js
@@ -3,19 +3,26 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(express.json())
 
 let user = [];
-let tweet = [];
 let tweets = [{
     username: "bobesponja",
     avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
     tweet: "eu amo o hub"
+}, {
+    username: "patrick",
+    avatar: "https://upload.wikimedia.org/wikipedia/pt/b/b1/Patrick_Estrela.png",
+    tweet: "aehoooooooooo"
+}, {
+    username: "lulamolusco",
+    avatar: "https://pbs.twimg.com/media/DWr8g-FW0AY5Bsm.jpg",
+    tweet: "eu odeio o hub"
 }];
 
 app.post("/sign-up", (req, res) => {
-    user.push(req.query);
-    console.log("Sign-up funcionando");
-    res.send("Sign-up funcionando");
+    user.push(req.body);
+    res.send(req.body.username);
 });
 
 app.get("/tweets", (req, res) => {
@@ -30,8 +37,20 @@ app.get("/tweets", (req, res) => {
         }
     }
     
-    console.log(tweetsToReturn, tweets.length)
     res.send(tweetsToReturn)
+})
+
+app.post("/tweets", (req, res) =>{
+    
+    let user1 = user.filter((object) =>{ return object.username === req.body.username})
+
+    let tweetToPost = {
+        username: user1[0].username,
+        avatar: user1[0].avatar,
+        tweet: req.body.tweet
+    };
+    tweets = [...tweets, tweetToPost]
+    res.send("Ok");
 })
 
 
